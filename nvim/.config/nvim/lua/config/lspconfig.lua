e5ab604609b2dc4ae684b1092a186e781eb20201
Martin Cespedes <mcespedescubillos@gmail.com>
2022-10-11 17:46:36 -0300

    Refactor completion plugin calls

--- nvim/.config/nvim/lua/config/lspconfig.lua
+++ nvim/.config/nvim/lua/config/lspconfig.lua
@@ -1,67 +1,3 @@
-local cmp = require'cmp'
-
-cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
-
-  -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
@@ -69,6 +5,7 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -96,6 +33,8 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
+capabilities = require("config.completion")
+
 require('lspconfig')['pyright'].setup{
     on_attach = on_attach,
     capabilities = capabilities,
@@ -107,21 +46,3 @@ require('lspconfig')['tsserver'].setup{
     capabilities = capabilities,
     flags = lsp_flags,
 }
-
--- require'lspconfig'.pyright.setup{
---     capabilities = capabilities,
---     on_attach = function()
---         vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
---         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
---         vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
---         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
---         vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
---         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer=0})
---         vim.keymap.set("n", "dj", vim.diagnostic.goto_next, {buffer=0})
---         vim.keymap.set("n", "dk", vim.diagnostic.goto_prev, {buffer=0})
---         vim.keymap.set("n", "dk", vim.diagnostic.goto_prev, {buffer=0})
---     end
--- }
--- require'lspconfig'.tsserver.setup{
---     capabilities = capabilities,
--- }
