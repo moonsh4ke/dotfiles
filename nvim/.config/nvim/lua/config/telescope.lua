89af35c6457135188805b65ada4de7f2279b44ac
Martin Cespedes <mcespedescubillos@gmail.com>
2022-10-31 20:07:05 -0300

    Shortcut for quick access to nvim files

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,5 +1,18 @@
 -- Telescope
-map = vim.api.nvim_set_keymap
+builtin = require("telescope.builtin")
+themes = require("telescope.themes")
+
+require('telescope').setup{
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    }
+	}
+}
+
+-- map = vim.api.nvim_set_keymap
+map = vim.keymap.set
 map('n', '<leader>.', '<cmd>Telescope find_files theme=dropdown<cr>', {})
 map('n', '<leader>,', '<cmd>Telescope buffers theme=dropdown<cr>', {})
 map('n', '<leader>:', '<cmd>Telescope builtin theme=dropdown<cr>', {})
+map('n', '<leader>n', function() builtin.find_files({cwd = "~/.config/nvim/lua"}) end, {})
