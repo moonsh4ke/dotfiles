51beb75c245c44b983f068662f1ed853999de3cc
Martin Cespedes <mcespedescubillos@gmail.com>
2022-11-17 23:55:45 -0300

    kuragawa theme

--- nvim/.config/nvim/lua/config/colorscheme.lua
+++ nvim/.config/nvim/lua/config/colorscheme.lua
@@ -5,14 +5,24 @@ vim.cmd([[
     let &t_8b="\<Esc>[48;2;%lu;%lu;%lum"
     set termguicolors
     endif
+		colorscheme kanagawa
 ]])
 
-vim.cmd[[let g:gruvbox_sign_column='bg0']]
-vim.cmd[[let g:gruvbox_contrast_dark = 'hard']]
-vim.cmd[[let g:gruvbox_invert_selection='0']]
-vim.cmd[[let g:gruvbox_italic=1]]
-vim.cmd[[set background=dark]]
-vim.cmd[[colorscheme gruvbox]]
+-- vim.cmd[[
+-- let ayucolor="light"  " for light version of theme
+-- let ayucolor="mirage" " for mirage version of theme
+-- let ayucolor="dark"   " for dark version of theme
+-- colorscheme ayu
+-- ]]
+
+vim.cmd[[
+	" let g:gruvbox_sign_column='bg0'
+	" let g:gruvbox_contrast_dark = 'hard'
+	" let g:gruvbox_invert_selection='0'
+	" let g:gruvbox_italic=1
+	" set background=dark
+	" colorscheme gruvbox
+]]
 
 -- Tokyonight
 -- vim.cmd[[colorscheme tokyonight]]
