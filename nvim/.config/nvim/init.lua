f711c90fda3ff1b22ad9a6ea198f4df0f55a1680
Martin Cespedes <mcespedescubillos@gmail.com>
2023-09-19 23:32:57 -0300

    packer removed

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,11 @@
 require('options') -- Setters
 require('keymaps') -- Keymaps
 require('autocmds') -- Autcommands
--- require('plugins') -- Plugins
 require('functions')
 require('scripts')
 
 -- Plugins
-require('plugins.lazy_bootstrap') -- load lazy into runtime
+require('plugins.lazy_bootstrap') -- load lazy into vim runtime path
 local plugins = require('plugins.lazy_plugins')
 require("lazy").setup({
 	plugins,
