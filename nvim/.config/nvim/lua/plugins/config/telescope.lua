89c79cd75c68911ca37a010c8ab420a001cf44f5
Martin Cespedes <mcespedescubillos@gmail.com>
2024-05-09 23:19:13 -0400

    telescope: added map to delete buffers & livegrep

--- nvim/.config/nvim/lua/plugins/config/telescope.lua
+++ nvim/.config/nvim/lua/plugins/config/telescope.lua
@@ -1,19 +1,28 @@
 -- Telescope
 builtin = require("telescope.builtin")
 themes = require("telescope.themes")
+local actions = require "telescope.actions"
 
 require('telescope').setup{
   pickers = {
     find_files = {
       theme = "dropdown",
+    },
+    buffers = {
+      mappings = {
+        n = {
+          ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+        }
+      }
     }
   }
 }
 
 -- map = vim.api.nvim_set_keymap
 map = vim.keymap.set
-map('n', '<leader>.', '<cmd>Telescope find_files theme=dropdown<cr>', {})
+map('n', '<leader>.', '<cmd>Telescope find_files theme=dropdown previewer=false<cr>', {})
 map('n', '<leader>,', '<cmd>Telescope buffers theme=dropdown<cr>', {})
-map('n', '<leader>:', '<cmd>Telescope builtin theme=dropdown<cr>', {})
+map('n', '<leader>:', '<cmd>Telescope builtin<cr>', {})
+map('n', '<leader>l', '<cmd>Telescope live_grep<cr>', {})
 map('n', '<leader>-', function() builtin.find_files({hidden=true, no_ignore=true}) end, {})
 map('n', '<leader>n', function() builtin.find_files({cwd = "~/.config/nvim/lua"}) end, {})
