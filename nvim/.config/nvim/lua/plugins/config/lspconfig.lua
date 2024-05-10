85ec0423b38845264f636b3d95aabaa73844471e
Martin Cespedes <mcespedescubillos@gmail.com>
2024-05-09 23:11:28 -0400

    updated lspconfig
    - currently not using tsserver
    - added java lsp
    - enabled pyright

--- nvim/.config/nvim/lua/plugins/config/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/config/lspconfig.lua
@@ -7,57 +7,25 @@ vim.diagnostic.config(
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set({'n', 'i'}, '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  -- vim.keymap.set('n', '<space>wl', function()
-  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  -- end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
-end
-
+local on_attach = require("plugins.config.lspconfig_on_attach")
 local capabilities = require("plugins.config.completion")
 
-require'lspconfig'.jedi_language_server.setup{
-  on_attach = on_attach,
-  capabilities = capabilities,
-  flags = lsp_flags
-}
-
--- require('lspconfig')['pyright'].setup{
+-- require'lspconfig'.jedi_language_server.setup{
 --   on_attach = on_attach,
 --   capabilities = capabilities,
 --   flags = lsp_flags
 -- }
 
-require('lspconfig')['tsserver'].setup{
-  on_attach = on_attach,
-  capabilities = capabilities,
-  flags = lsp_flags
-}
+-- require('lspconfig')['tsserver'].setup{
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   flags = lsp_flags
+-- }
 
 require'lspconfig'.cssls.setup{
   on_attach = on_attach,
@@ -72,6 +40,8 @@ require'lspconfig'.bashls.setup{
 }
 
 require'lspconfig'.lua_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
   settings = {
     Lua = {
       runtime = {
@@ -94,39 +64,26 @@ require'lspconfig'.lua_ls.setup {
   },
 }
 
--- require'lspconfig'.sumneko_lua.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   flags = lsp_flags,
---   settings = {
---     Lua = {
---       runtime = {
---         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
---         version = 'LuaJIT',
---       },
---       diagnostics = {
---         -- Get the language server to recognize the `vim` global
---         globals = {'vim', 'awesome', 'client', 'root'},
---       },
---       workspace = {
---         -- Make the server aware of Neovim runtime files
---         library = vim.api.nvim_get_runtime_file("", true),
---       },
---       -- Do not send telemetry data containing a randomized but unique identifier
---       telemetry = {
---         enable = false,
---       },
---     },
---   },
--- }
-
+require'lspconfig'.jdtls.setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags
+}
 
 require'lspconfig'.emmet_ls.setup{
+  -- on_attach = on_attach,
+  capabilities = capabilities,
   cmd  = { "emmet-ls", "--stdio" },
-  filetypes = { "ejs", "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "eruby" },
+  filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
 }
 
 require'lspconfig'.clangd.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+  flags = lsp_flags
+}
+
+require'lspconfig'.pyright.setup{
   on_attach = on_attach,
   capabilities = capabilities,
   flags = lsp_flags
